2f906bf934833b9efa94f68ea4f900e98e5185f4
Mattox0 <matbegaaar@gmail.com>
2023-04-17 00:18:19 +0200

    Change classe

--- src/class/dto/classe.dto.ts
+++ src/class/dto/classe.dto.ts
@@ -3,8 +3,7 @@ import { IsEmail, IsNotEmpty, MinLength, IsInt } from "class-validator"
 export class CreatedClasseDto {
     @IsNotEmpty()
     name: string;
-
-    @IsInt()
+    
     @IsNotEmpty()
     dice: number;
 
